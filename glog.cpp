8447ebaa9c6bd112cd0ff0d78ef2e8714c322bb9
Sandeep Prakash <123sandy@gmail.com>
2017-10-22 22:13:38 -0700

    Removed test code

--- glog.cpp
+++
@@ -1,9 +0,0 @@
-#include <glog/logging.h>
-
-   int main(int argc, char* argv[]) {
-     // Initialize Google's logging library.
-     google::InitGoogleLogging(argv[0]);
-
-     // ...
-     LOG(INFO) << "Found  cookies";
-   }
