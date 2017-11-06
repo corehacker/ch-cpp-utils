950ad9ec3410bbe28898e39891758b51052243f3
Sandeep Prakash <123sandy@gmail.com>
2017-11-05 20:53:53 -0800

    Timer wrapper around implementation with libevent.

---
+++ test/test-timer.cpp
@@ -0,0 +1,14 @@
+/*
+ * test-timer.cpp
+ *
+ *  Created on: Nov 5, 2017
+ *      Author: corehacker
+ */
+
+#include <glog/logging.h>
+#include "ch-cpp-utils/timer.hpp"
+
+
+int main(int argc, char **argv) {
+	return 0;
+}
